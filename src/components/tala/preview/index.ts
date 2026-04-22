3858288ab3eb56220a73590538e2298ae6a99d11
carlofelipe-hub <carlofelipe101@gmail.com>
2026-04-23 07:11:27 +0800

    feat(preview): add barrel export

---
+++ src/components/tala/preview/index.ts
@@ -0,0 +1,7 @@
+export { PreviewClient } from "./preview-client";
+export { TalaResumeDoc } from "./tala-resume-doc";
+export { TemplatePicker } from "./template-picker";
+export { FineTunePanel } from "./fine-tune-panel";
+export { Knob } from "./knob";
+export { AtsScoreBox } from "./ats-score-box";
+export { ResumeSection } from "./resume-section";
